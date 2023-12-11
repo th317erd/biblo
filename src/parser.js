6470960d42cd3293bae43791ba9c0dc7d855dae6
Wyatt Greenway <th317erd@gmail.com>
2023-12-10 20:47:41 -0700

    Update parser to strip beginning of lines

--- src/parser.js
+++ src/parser.js
@@ -10,60 +10,99 @@ import {
   buildHelperRunner,
 } from './helpers/index.js';
 
-const BLOCK_PATTERN = /\/\*\*\*[\s\S]*?\*\*\*\//g;
+const BLOCK_PATTERN         = /\/\*\*\*([\s\S]*?)\*\*\*\//g;
+const BLOCK_LINE_START_SKIP = /^[^0-Za-z"'-]+/;
 
 /***
-groupName: Parser
-desc: |
-  Parse all comment blocks out of a text file.
-  A `block` is simply a block comment in C/Javascript
-  style, except that you must use three asterisks on
-  each side of the block (instead of only one).
-
-  Example:
-  ```javascript
-  /&ast;&ast;&ast;
-  desc: This is a block with a "desc" key
-  &ast;&ast;&ast;/
-  function doSomething() {}
-  ```
-
-  Normally one is unable to use things like grave accents "`"
-  in YAML, but Biblo does some magic parsing and replacing for
-  you so that Markdown format is easier to use in these block
-  comments.
-args:
-  content:
-    desc: Content string to parse blocks out of
-    type: string
-return:
-  type: Array[string]
-  desc: |
-    Return an array of parsed blocks.
-
-    Blocks have the shape `{ content: string, start: number, end: number }`,
-    where `start` and `end` represent the start and end indexes of the parsed
-    block (respectively).
-
-    `content` is the content of the parsed block.
+ * groupName: Parser
+ * desc: |
+ *   Parse all comment blocks out of a text file.
+ *   A `block` is simply a block comment in C/Javascript
+ *   style, except that you must use three asterisks on
+ *   each side of the block (instead of only one).
+ *
+ *   Example:
+ *   ```javascript
+ *   /&ast;&ast;&ast;
+ *   desc: This is a block with a "desc" key
+ *   &ast;&ast;&ast;/
+ *   function doSomething() {}
+ *   ```
+ *
+ *   Normally one is unable to use things like grave accents "`"
+ *   in YAML, but Biblo does some magic parsing and replacing for
+ *   you so that Markdown format is easier to use in these block
+ *   comments.
+ * args:
+ *   content:
+ *     desc: Content string to parse blocks out of
+ *     type: string
+ * return:
+ *   type: Array[string]
+ *   desc: |
+ *     Return an array of parsed blocks.
+ *
+ *     Blocks have the shape `{ content: string, start: number, end: number }`,
+ *     where `start` and `end` represent the start and end indexes of the parsed
+ *     block (respectively).
+ *
+ *     `content` is the content of the parsed block.
 ***/
-export function parseBlocks(content) {
-  const formatContent = (content) => {
-    return content
+export function parseBlocks(content, _options) {
+  let options = _options || {};
+
+  const formatContent = (_content) => {
+    let content = _content;
+
+    let minLineStart = findMinLineStart(content);
+    if (minLineStart > 0)
+      content = content.split(/\r\n|\r|\n/g).map((line) => line.substring(minLineStart)).join('\n');
+
+    content = content
       .replace(/^\/\*+\s*/, '')
       .replace(/\s*\*+\/$/, '')
       .replace(/&ast;/g, '*')
       .replace(/`/g, '&grave;');
+
+    return content;
   };
 
-  let blocks = [];
+  const getBlockPattern = () => {
+    if (options.blockPattern) {
+      if (Utils.isType(options.blockPattern, RegExp, 'RegExp'))
+        return options.blockPattern;
+      else if (Utils.isType(options.blockPattern, 'String'))
+        return new RegExp(options.blockPattern, 'gm');
+    }
+
+    return BLOCK_PATTERN;
+  };
+
+  const findMinLineStart = (block) => {
+    return block.split(/\r\n|\r|\n/g).reduce((min, line) => {
+      let toStartOfLine = Infinity;
+      line.replace(BLOCK_LINE_START_SKIP, (m) => {
+        if (m === line) // If whole line was matched, then skip
+          return m;
+
+        toStartOfLine = m.length;
+
+        return m;
+      });
+
+      return (toStartOfLine < min) ? toStartOfLine : min;
+    }, Infinity);
+  };
+
+  let blockPattern  = getBlockPattern();
+  let blocks        = [];
   let previousBlock;
 
-  content.replace(BLOCK_PATTERN, (m, offset) => {
+  content.replace(blockPattern, (m, block, offset) => {
     let newBlock = {
       start:    offset,
       end:      offset + m.length,
-      content:  formatContent(m),
+      content:  formatContent(block),
     };
 
     if (previousBlock) {
@@ -87,7 +126,7 @@ export function parse(source, _options) {
   };
 
   let options = _options || {};
-  let blocks  = parseBlocks(source);
+  let blocks  = parseBlocks(source, options);
 
   return blocks.map((block) => {
     let scope;
